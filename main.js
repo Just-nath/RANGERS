c7ffc034d72f4bd3b769fcb1e0449693300699eb
CodeSandbox <support@codesandbox.io>
2024-12-24 00:13:24 +0000

    Initial commit for RANGERS project

---
+++ main.js
@@ -0,0 +1,41 @@
+const warpcastFrame = {
+    user: {
+        username: "@schatpunk",
+        rank: 1329,
+        fid: 451136,
+    },
+    allowance: {
+        remaining: 1158,
+        tomorrowEstimated: 1158,
+    },
+    points: {
+        total: 1636,
+        tipped: 0,
+    },
+    reloads: {
+        in: "22 hours 10 minutes",
+    },
+    locked: {
+        today: "754M",
+        tomorrow: "754M",
+    },
+    openrankPoints: 276,
+    unitsStats: {
+        totalUnits: 1000, // Example value
+        activeUnits: 500,  // Example value
+    },
+    maxAllowance: {
+        additional: 1629713,
+        total: 1629713,
+    },
+    tips: {
+        givenToday: 0,
+        reminder: "Don't forget to tip @just-nath using the button below.",
+    },
+    advice: "Don't give up.",
+    degenStats: {
+        createdBy: "@just-nath",
+        createdAt: "2024-23-12 01:49:48 UTC",
+    },
+};
+
